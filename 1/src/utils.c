762d6f90f4423d8ff16eeb193f63432125177be0
Алексей Галов <jifaxin@niuitmo.ru>
2025-11-02 16:21:22 +0300

    tests and make write funtions always BE

--- 1/src/utils.c
+++ 1/src/utils.c
@@ -8,11 +8,17 @@ void write_uint8(FILE* file, uint8_t num) {
 }
 
 void write_uint32(FILE* file, uint32_t num) {
-	fwrite(&num, 4, 1, file);
+	unsigned char buf[4];
+	for (int i = 0; i < 4; i++)
+		buf[i] = (num >> i * 8) & 0xff;
+	fwrite(buf, 4, 1, file);
 }
 
 void write_double(FILE* file, double num) {
-	fwrite(&num, sizeof(num), 1, file);
+	unsigned char buf[8];
+	for (int i = 0; i < 8; i++)
+		buf[i] = (*((uint64_t*)&num) >> i * 8) & 0xff;
+	fwrite(buf, 8, 1, file);
 }
 
 void write_string(FILE* file, char* str) {
